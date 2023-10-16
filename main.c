47341c31f68a67c35edf65dd97c81aa1ca2f48d9
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-10-16 13:37:16 +0300

    finished creating function to read user input for grade percentages

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+
+// reads user's grade percentage for a certain subject
+
+int input_grade_percentage(const char* subject)
+{
+    int grade_percentage = 0;
+
+    while (1)
+    {
+        printf("\nEnter your grade for %s (0 - 100): ", subject);
+
+        if (scanf("%d", &grade_percentage) != 1)
+        {
+            printf("Incorrect input. Please type an integer.\n");
+            while(getchar() != '\n');
+        }
+
+        else
+        {
+            return grade_percentage;
+        }
+    }
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+
+    printf("Welcome to the Student Grade Calculator!\n");
+
     return 0;
 }
